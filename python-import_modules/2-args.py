45d778e0ae45a6fabb64a70ddd69e8675bfedbf0
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-20 23:24:43 +0100

    adding args file

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    for i in range(0, len(sys.argv)):
+        i += 1
+
+    i = i - 1
+
+    if i == 1:
+        print("{} argument:".format(i))
+    elif i == 0:
+        print("{} arguments.".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    for x in range(0, len(sys.argv)):
+        if x != 0:
+            print("{}: {}".format(x, sys.argv[x]))
