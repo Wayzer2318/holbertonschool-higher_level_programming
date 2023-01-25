d668da1fd57ef8602686c42737006604b69a72c3
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-25 23:10:24 +0100

    fixed bug

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return
     mv = 0
-    for i in range(1, my_list):
+    for i in range(1, len(my_list)):
         if i > mv:
             mv = i
     return mv
