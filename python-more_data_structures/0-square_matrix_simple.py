46c9c37e5c967796d41cd5f85fdb048073b6dc67
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-26 10:53:36 +0100

    fixed bug

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@
 def square_matrix_simple(matrix=[]):
     nm = []
     for i in range(len(matrix)):
-        new.append(list(map((lambda x: x ** 2), matrix[i])))
+        nm.append(list(map((lambda x: x ** 2), matrix[i])))
     return nm
