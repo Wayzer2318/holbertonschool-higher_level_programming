3f403094c482d059b537e711c833a9ac64492134
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-30 14:21:43 +0100

    fixed bug

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,8 @@
 class Square:
     """ square """
     def __init__(self, size=0):
-        self.__size = size
-    
+        self.__size = size    
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-
