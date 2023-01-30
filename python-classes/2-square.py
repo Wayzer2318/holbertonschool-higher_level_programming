43596d4e5cfa813dbb1c0ad403a344e08ab4e452
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-30 14:07:44 +0100

    adding task 3

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" square """
+
+
+class Square:
+    """ square """
+    def __init__(self, name):
+        self._Square__size = name
+
+p = Square('size')
+
+try:
+    type(size) = int
+except TypeError:
+    print("size must be an integer")
+
+try:
+    size < à
+except ValueError:
+    print("size must be >= 0")
