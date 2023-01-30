e6067637ccc4cdb599277a3df67cf9f1046e7dd2
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-30 04:18:44 +0100

    fixed bug

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,12 +2,4 @@
 
 
 def only_diff_elements(set_1, set_2):
-    set_1 = set(set_1)
-    set_2 = set(set_2)
-    
-    for i in set_2:
-        if i not in set_1:
-            return i
-    for i in set_1:
-        if i not in set_2:
-            return i
+    return (set_1 ^ set_2)
