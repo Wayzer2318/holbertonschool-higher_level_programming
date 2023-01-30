b5a87b69d7144226362af99f35f0acc43520a8f2
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-30 12:19:17 +0100

    adding task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    writing a class that define square
+"""
+
+
+class Square:
+    """
+        define the square class
+    """
+    __init__(self, name):
+        self._Square__size = name
