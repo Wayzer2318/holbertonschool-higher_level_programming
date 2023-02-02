7030e7362d0f9ab77a0d30044637c36a8ce851d8
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-02 03:09:21 +0100

    adding task 4

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Square """
+class Square:
+    """ Square """
+    def __init__(self, size=0):
+        self.name = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if rize < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return size**size
+    
