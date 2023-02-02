0962369d670968fdd09f6b4406846fc9c38079fa
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-02 10:49:28 +0100

    fixed bug

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    if a is not int:
+    if type(a) != int:
         raise TypeError('a must be an integer')
-    if b is not int:
+    if type(b) != int:
         raise TypeError('b must be an integer')
-    a = int(a)
-    b = int(b)
     return a + b
