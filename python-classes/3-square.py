39801baa1e1364065efc0b529cfbb6ea66448f9d
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-02 03:10:27 +0100

    fixed bug

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
         self.name = size
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if rize < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
