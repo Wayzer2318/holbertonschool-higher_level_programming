05c21fdb7de9244d53e58b10fece53b40d4dadd2
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-06 14:43:49 +0100

    adding task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" text indentation """
+
+
+def text_indentation(text):
+    """ text indentation """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
+        i = 0
+        for k in text:
+            if i == 0:
+                if k == " ":
+                    continue
+                else:
+                    i = 1
+            if i == 1:
+                if k == '.' or k == '?' or k == ':':
+                    print(k + '\n')
+                    i = 0
+                else:
+                    print(k, end="")
