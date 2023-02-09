f7d049187b91df2184bbd3e71f11fa1ffe1de72c
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-09 18:13:32 +0100

    fixed name error

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -74,7 +74,7 @@ class Rectangle():
         print("Bye rectangle...")
 
     @staticmethod
-    def bigger_or_equial(rect_1, rect_2):
+    def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
