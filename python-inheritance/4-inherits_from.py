79f0d01eefeab61120ed51571e978794ebcc92f9
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-10 21:41:38 +0100

    fixed name error

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,6 +2,6 @@
 """ inherit or instance """
 
 
-def inherits_form(obj, a_class):
+def inherits_from(obj, a_class):
     """ inherit or instance """
     return type(obj) is not a_class and isinstance(obj, a_class)
