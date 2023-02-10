c1c0d5fc8a240f9ace15b2b63d6255baa929f14b
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-10 23:42:59 +0100

    fixed bug

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ rectangle """
 
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-class Rectangle:
+
+class Rectangle(BaseGeometry):
     """ rectangle """
     def __init__(self, width, height):
         self.__width = width
