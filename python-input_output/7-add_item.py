61efd7c1e0888c66895b7fc430f803af4f4dddaa
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-10 23:57:30 +0100

    adding task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" save """
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    filename = "add_item.json"
+    with open(filename, 'a+', encoding="utf-8") as f:
+        try:
+            my_list = load_from_json_file(filename)
+        except:
+            my_list = []
+        my_list.extend(args[1:])
+        save_to_json_file(my_list, filename)
