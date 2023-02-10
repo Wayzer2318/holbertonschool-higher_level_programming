5d842636f6226927a0c61400976d60fc0af968aa
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-11 00:01:16 +0100

    adding task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" class student """
+
+
+class Student:
+    """ class student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+        else:
+            return {k: v for k, v in vars(self).items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for key in json:
+            setattr(self, key, json[key])
