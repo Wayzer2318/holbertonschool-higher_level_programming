8c3e7906c4ae35db3a0afd951e5baf229add894c
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-10 23:31:58 +0100

    adding task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" rectangle """
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
