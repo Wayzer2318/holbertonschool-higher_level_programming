501c18d66e80d6393ccff2c6cde4b6ee24463d14
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-13 00:23:20 +0100

    fixed name bug

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 
 
 def multiply_by_2(a_dictonary):
-    new_dictionary = a_dictonary
+    new_dictionary = a_dictionary
     for key in new_dictionary:
         new_dictionary[key] *= 2
     return new_dictionary
