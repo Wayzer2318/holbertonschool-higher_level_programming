253aea17bfab04e7d3a73ff1624f64d2b473aac7
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-13 18:20:57 +0100

    fixed bug

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,6 @@
 
 
 def best_score(a_dictionary):
-    result = 0
-    for value in a_dictionary:
-        if value > result:
-            result = value
-    return result
+    if not a_dictionary:
+        return(None)
+    return (max(a_dictionary, key=a_dictionary.get))
