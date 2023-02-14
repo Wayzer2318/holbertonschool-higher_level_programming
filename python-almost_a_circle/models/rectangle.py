589a2a3de07443afb14885a3a5a9335ab0302506
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-14 05:44:13 +0100

    adding rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from base import Base
+
+""" rectangle """
+
+
+class Rectangle(Base):
+    """ rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """The width property."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
+
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
