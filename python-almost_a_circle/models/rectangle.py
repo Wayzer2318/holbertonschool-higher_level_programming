02bc25b0fd9d74a6a6af24f12ac0a089d6568b9a
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-14 14:04:58 +0100

    fixed bug

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """ str """
-        return ("{} {} {}/{} - {}/{}".format("[Rectangle]", self.id, self.x, self.y, self.width, self.height))
+        return ("{} ({}) {}/{} - {}/{}".format("[Rectangle]", self.id, self.x, self.y, self.width, self.height))
 
     @property
     def width(self):
