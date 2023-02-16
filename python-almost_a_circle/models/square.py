ad3a591ea730f976ae0c36de04eacc4b4f352ef6
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-16 17:14:28 +0100

    fixed bug

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-""" square """
+""" First Square """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Square """
+    """ square """
+
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """The size property."""
         return self.width
 
     @size.setter
     def size(self, size):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be > 0")
-        self.height = size
         self.width = size
+        self.height = size
 
     def __str__(self):
         """ str """
-        ident = self.id
+        identifier = self.id
         size = self.width
         x = self.x
         y = self.y
 
-        return f"[Square] ({ident}) {x}/{y} - {size}"
+        return f"[Square] ({identifier}) {x}/{y} - {size}"
