402a0943bc08f117a6539dcaf786d751176b52b6
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-16 16:16:49 +0100

    fixed errors

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ square """
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Square """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(id)
         super().__init__(x)
