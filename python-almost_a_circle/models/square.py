0c80e56663c97b44a849e6936a367afda38da3ed
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-02-16 16:24:28 +0100

    fixed errors

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,14 +6,14 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Square """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id)
-        super().__init__(x)
-        super().__init__(y)
-        super().__init__(size, size)
         self.size = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ str """
-        str = ("[Square] (%s) %s/%s - %s/%s" %
-               (self.id, self.x, self.y, self.size, self.size))
-        return str
+        ident = self.id
+        size = self.height
+        x = self.x
+        y = self.y
+
+        return f"[Square] ({ident}) {x}/{y} - {size}"
