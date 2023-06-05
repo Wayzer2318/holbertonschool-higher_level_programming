1b0c8fd30d6946dbe962a015d209f3c9cf9f071d
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-05 15:26:08 +0200

    adding task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        copy = my_list
-        return copy
+        copy1 = my_list.copy()
+        return copy1
     elif idx not in range(len(my_list)):
-        copy = my_list
-        return copy
-    my_list[idx] = element
-    return my_list
+        copy2 = my_list.copy()
+        return copy2
+    copy3 = my_list.copy()
+    copy3[idx] = element
+    return copy3
