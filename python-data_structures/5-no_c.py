0106e342585b9a5e16e8f46d4fef018d0dcd86d4
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-05 16:29:26 +0200

    fix bug

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    string = ""
     for i in my_string:
-        if i == 99:
-            my_string[i] = " "
-        if i == 67:
-            my_string[i] = " "
+        if i != 'c' and i != 'C':
+            string += i
+    return string
