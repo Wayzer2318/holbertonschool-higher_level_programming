fbe2c91b838c86bcdafcc3aa7a4e8434361b7d29
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-05 15:32:29 +0200

    fix syntax

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     for i in my_string:
         if i == "c":
-            my_string[i] = ""
+            my_string[i] = " "
         if i == "C":
-            my_string[i] = ""
+            my_string[i] = " "
