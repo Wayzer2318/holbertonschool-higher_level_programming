76b612171a6a59b91fe2ab7a4a7c35f4aa44be62
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-05 14:45:17 +0200

    fix syntax

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
-    elif idx >= len(my_list):
+    elif idx not in range(len(my_list)):
         return(my_list)
     else:
-        retturn(my_list[:idx]
+        my_list[idx] = element
+        return(my_list)
