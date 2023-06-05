bf761a36b676c0d31b4e8daed7603072e2d67b88
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-05 16:20:11 +0200

    adding ascii

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for i in my_string:
-        if i == "c":
+        if i == 99:
             my_string[i] = " "
-        if i == "C":
+        if i == 67:
             my_string[i] = " "
