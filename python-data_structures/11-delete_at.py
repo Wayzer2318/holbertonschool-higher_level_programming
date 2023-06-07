a4f58a21a1226671a1ba46b7a09001c094275ea8
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-07 03:26:09 +0200

    adding task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    my_list.pop(idx)
+    if idx >= len(my_list):
+        return my_list
+    for i in range(len(my_list)):
+        if i == idx:
+            del my_list[i]
     return my_list
