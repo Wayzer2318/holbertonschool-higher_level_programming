b24741c484c27474b869819d4176e54d40b84ccd
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-07 03:06:29 +0200

    adding task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_value = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_value:
+            max_value = my_list[i]
+    return max_value
