b451396c473fe8975c12e660a169efb92f607e87
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-08 11:22:29 +0200

    adding permission

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_key = max(a_dictionary, key = a_dictionary.get)
-    return max_key
+    if not a_dictionary:
+        return None
+    return max(a_dictionary.keys())
