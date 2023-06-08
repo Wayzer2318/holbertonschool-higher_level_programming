2537374c4ebd83932537cf8084562a7dd9da543b
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-08 13:45:19 +0200

    fix bug

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,12 +9,14 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
+    if roman_string is None:
+        return 0
     if type(roman_string) is not str:
         return 0
     roman_int = 0
     for i in range(len(roman_string) - 1):
         if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            roman_int += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i + 1]]
+            roman_int += roman_dict[roman_string[i]] - 2
         else:
             roman_int += roman_dict[roman_string[i]]
     roman_int += roman_dict[roman_string[-1]]
