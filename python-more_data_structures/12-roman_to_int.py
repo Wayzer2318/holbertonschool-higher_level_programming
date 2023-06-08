774405b2399b88657a1fcde833e7243369561a76
Arezki Ait Kettout <wayzer@Arezki.local>
2023-06-08 13:39:00 +0200

    adding task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    if not roman_string:
+        return 0
+    roman_int = 0
+    for i in range(len(roman_string) - 1):
+        if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+            roman_int += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i + 1]]
+        else:
+            roman_int += roman_dict[roman_string[i]]
+    roman_int += roman_dict[roman_string[-1]]
+    return roman_int
