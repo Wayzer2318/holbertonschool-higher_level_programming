8f2e0c7ac785b8b7d819e9d8d6d6a9c874359b9b
Wayzer2318 <5820@holbertonstudents.com>
2023-06-09 13:03:17 +0200

    adding task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while True:
+        try:
+            while i != x:
+                print("{}".format(my_list[i]), end="")
+                i += 1
+        except:
+            pass
+        finally:
+            print()
+            return i
