329e68c9f7d5c420e05be9d7a92f07416d064d67
Wayzer2318 <5820@holbertonstudents.com>
2023-06-12 10:49:12 +0200

    adding tak 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define a class Square
+"""
+
+
+class Square:
+    """
+    square class
+    """
+    def __init__(self,size):
+        """
+        init square with size
+        """
+        self.__size = size
