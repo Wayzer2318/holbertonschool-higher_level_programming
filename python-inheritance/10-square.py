5854a6880eeca95f99d54024bb5a1cbaab043588
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-06-28 13:25:12 +0200

    adding task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Base Geometry """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """define area function"""
+        return self.__size ** 2
