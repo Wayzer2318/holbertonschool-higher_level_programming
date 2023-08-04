d53818b68e1d803bccba16eca76f94fd3fd3d010
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-08-04 20:33:57 +0200

    adding task 7

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""
-displays all values in
-table where name matche argument
-"""
-
+'''
+Script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+'''
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-            host="localhost",
-            port=3306, 
-            user=sys.argv[1],               
-            passwd=sys.argv[2],
-            db=sys.argv[3])
-
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states  ORDER BY id ASC")
-        for row in cur.fetchall():
-            if row[1] == sys.argv[4]:
-                print(row)
-
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8"
+                           )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
     conn.close()
